6573de028da4c0036605eb8f201e9ac862315060
Sivan Wolberg <Sivan@wolberg.pro>
2020-02-09 18:38:36 +0200

    rebuild commands and added cleam for dist folder

--- backend/apps/front-api/webpack.config.js
+++ backend/apps/front-api/webpack.config.js
@@ -3,10 +3,20 @@ const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const isProduction =
+  typeof process.env.NODE_ENV !== 'undefined' &&
+  process.env.NODE_ENV === 'production';
+const mode = isProduction ? 'production' : 'development';
+const devtool = isProduction ? false : 'inline-source-map';
 const appPath = './apps/front-api';
 module.exports = {
   entry: ['webpack/hot/poll?100', `${appPath}/src/main.ts`],
-  watch: true,
+  optimization: {
+    minimize: false,
+  },
+  mode,
+  devtool,
   target: 'node',
   externals: [
     nodeExternals({
@@ -22,14 +32,22 @@ module.exports = {
       },
     ],
   },
-  mode: 'development',
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     plugins: [
       new TsconfigPathsPlugin({ configFile: `${appPath}/tsconfig.app.json` }),
     ],
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
+    new ForkTsCheckerWebpackPlugin({
+      tslint: true,
+    }),
+    new webpack.DefinePlugin({
+      CONFIG: JSON.stringify(require('config')),
+    }),
+  ],
   output: {
     path: path.join(__dirname, '../', '../', 'dist', 'apps', 'front-api'),
     filename: 'server.js',
