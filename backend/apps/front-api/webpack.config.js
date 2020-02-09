254c3df86a1f07410148f6f1a73f45d08b2ab5d2
Sivan Wolberg <Sivan@wolberg.pro>
2020-02-09 15:49:36 +0200

    need fix the pathing within the webpack

--- backend/apps/front-api/webpack.config.js
+++ backend/apps/front-api/webpack.config.js
@@ -2,9 +2,10 @@
 const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const appPath = './apps/front-api';
 module.exports = {
-  entry: ['webpack/hot/poll?100', './apps/front-api/src/main.ts'],
+  entry: ['webpack/hot/poll?100', `${appPath}/src/main.ts`],
   watch: true,
   target: 'node',
   externals: [
@@ -25,9 +26,12 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new TsconfigPathsPlugin({ configFile: `${appPath}/tsconfig.app.json` }),
+  ],
   output: {
-    path: path.join(__dirname,'../','../', 'dist','apps','front-api'),
+    path: path.join(__dirname, '../', '../', 'dist', 'apps', 'front-api'),
     filename: 'server.js',
   },
 };
