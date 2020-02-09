7ceb6d1b3e5dcd4682a053b4998face1eeed447a
Sivan Wolberg <Sivan@wolberg.pro>
2020-02-09 05:43:15 +0200

    added i18n support

--- backend/common/modules/commonModule.ts
+++ backend/common/modules/commonModule.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import {LoggerModule} from './loggerModule';
+import * as path from 'path';
+import { I18nModule, QueryResolver, HeaderResolver } from 'nestjs-i18n';
 @Module({
-  imports: [ConfigModule, LoggerModule],
+  imports: [ConfigModule, LoggerModule,
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        path: path.join(__dirname, configService.get('i18n.path')),
+        fallbackLanguage: configService.get('i18n.fallbackLanguage'), // e.g., 'en'
+        filePattern: configService.get('i18n.patten'), // e.g., '*.i18n.json'
+      }),
+      resolvers: [
+        { use: QueryResolver, options: ['lang', 'locale', 'l'] },
+        HeaderResolver,
+      ],
+      inject: [ConfigService],
+    })
+  ],
   providers: [],
   exports: [],
 })
